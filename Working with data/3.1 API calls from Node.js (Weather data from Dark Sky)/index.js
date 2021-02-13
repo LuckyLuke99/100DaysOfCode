c04d68ff364c307c502d145e37832e670b38b10c
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2021-02-13 11:53:22 -0300

    Making 3.1 WOrking with Data and Api of The Code Train

---
+++ Working with data/3.1 API calls from Node.js (Weather data from Dark Sky)/index.js
@@ -0,0 +1,43 @@
+const { request, response } = require('express');
+const express = require('express');
+const Datastore = require('nedb');
+const fetch = require('node-fetch');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+//Getting the data of database
+app.get('/database', (request, response) => {
+    database.find({}, (err, data) =>{
+        if(err){
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
+//Receiving the data and put in the database
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
+//Getting data from Api Open Weather
+app.get('/weather/:latlon', async (request, response) => {
+    const latlon = request.params.latlon.split(',');
+    const lat = latlon[0];
+    const lon = latlon[1];
+    const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=27213ca3a94758456ef6e9a2068714fb`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
