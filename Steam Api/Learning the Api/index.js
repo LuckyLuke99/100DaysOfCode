c7a53d8c72b0209d300e1a904cc8c6a18030473e
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2021-02-19 09:14:32 -0300

    Sorting out getting_items of the update_items

--- Steam Api/Learning the Api/index.js
+++ Steam Api/Learning the Api/index.js
@@ -31,17 +31,13 @@ app.get('/update_items/:colletion', async (request, response) => {
     const item_colletion = request.params.colletion;
     const item_count = 50;
     const item_data = [];
-    
-    let temp_data;
+    const interval_time = 3000;
+
     let item_start = 0;
     let item_loop = true;
 
-    setInterval()
     while(item_loop){
-        const item_url = `https://steamcommunity.com/market/search/render/?&currency=7&search_descriptions=0&sort_column=name&sort_dir=desc&appid=730&norender=1&count=${item_count}&start=${item_start}&category_730_ItemSet%5B%5D=tag_set_${item_colletion}`
-        const item_response = await fetch(item_url);
-        temp_data = await item_response.json();
-        
+        const temp_data = await getting_items(item_count, item_start, item_colletion);
         for (item of temp_data.results){
             item_data.push(item);
         }
@@ -49,7 +45,7 @@ app.get('/update_items/:colletion', async (request, response) => {
         console.log(item_start);
         if(temp_data.results.length != item_count){
             item_loop = false;
-        } 
+        }
     }
 
     const data = {
@@ -58,3 +54,11 @@ app.get('/update_items/:colletion', async (request, response) => {
     }
     response.json(data);
 })
+
+async function getting_items(item_count, item_start, item_colletion){
+    const item_url = `https://steamcommunity.com/market/search/render/?&currency=7&search_descriptions=0&sort_column=name&sort_dir=desc&appid=730&norender=1&count=${item_count}&start=${item_start}&category_730_ItemSet%5B%5D=tag_set_${item_colletion}`
+    const item_response = await fetch(item_url);
+    temp_data = await item_response.json();
+
+    return temp_data;
+}
